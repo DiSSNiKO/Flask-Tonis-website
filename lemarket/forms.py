e6a292b3668dd9ec126fc583cf64dcf54d34b359
DiSSNiKO <nikokalanda@gmail.com>
2022-05-23 22:10:41 +0400

    reg 1.0

--- lemarket/forms.py
+++ lemarket/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email = StringField(label='email')
-    password1 = StringField(label='password')
-    password2 = StringField(label='repeat password')
-    submit = SubmitField(label='submit')
+    username = StringField(label='username', validators=[Length(min=2,max=30), DataRequired()])
+    email = StringField(label='email', validators=[Length(min=10,max=50), DataRequired()])
+    password1 = PasswordField(label='password',validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='repeat password', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
--- lemarket/routes.py
+++ lemarket/routes.py
@@ -1,7 +1,14 @@
+from sqlalchemy import true
 from lemarket import webapp
 from lemarket.forms import RegisterForm
-from flask import render_template
-from lemarket.models import itemData
+from flask import render_template, redirect, url_for, flash
+from lemarket import db
+from lemarket.models import itemData, User
+def isInDbase(newname):
+        user = User.query.filter_by(username=newname).first()
+        if user!=None:
+            return True
+        return False
 @webapp.route("/")
 @webapp.route("/home") 
 def home_page():
@@ -11,9 +18,22 @@ def home_page():
 def market():
     item = itemData.query.all()
     return render_template('market.html', items=item)
-@webapp.route("/register")
+@webapp.route("/register", methods=["GET","POST"])
 def register_page():
     form = RegisterForm()
-    return render_template('register.html', usableForm = form)
+    registerfail = False
+    
+    if form.validate_on_submit():
+        print(f"wtf is this {isInDbase(form.username.data)}")
+        if isInDbase(form.username.data)==False:
+            user_to_add = User(username=form.username.data, email= form.email.data, password_hash = form.password1.data)
+            db.session.add(user_to_add)
+            db.session.commit()
+            return redirect(url_for('home_page'))
+        else:
+            registerfail = true
+    if form.errors!={}:
+        registerfail = true
+    return render_template('register.html', usableForm = form, registerfailed=registerfail)
 if __name__ == "__main__":
     webapp.run(debug=True)
